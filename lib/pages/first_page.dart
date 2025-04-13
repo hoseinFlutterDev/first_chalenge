879f292e29047d87273f710e1552cce7283d25eb
hoseinFlutterDev <hosein.flutter@gmail.com>
2025-04-13 20:19:11 +0330

    add pazel

--- lib/pages/first_page.dart
+++ lib/pages/first_page.dart
@@ -177,21 +177,22 @@ class _FirstPageState extends State<FirstPage> {
                         vertical: 10,
                       ),
                       child: Container(
-                        height: 120,
+                        height: 110, // کاهش ارتفاع برای جا شدن کامل 6 عکس
                         decoration: BoxDecoration(
                           border: Border.all(color: Colors.white24),
                           borderRadius: BorderRadius.circular(10),
                           color: Color.fromARGB(255, 33, 31, 49),
                         ),
                         child: GridView.builder(
-                          padding: const EdgeInsets.all(8),
-                          gridDelegate:
-                              SliverGridDelegateWithFixedCrossAxisCount(
-                                crossAxisCount: 3,
-                                crossAxisSpacing: 8,
-                                mainAxisSpacing: 8,
-                                childAspectRatio: 1,
-                              ),
+                          padding: const EdgeInsets.all(6),
+                          physics: NeverScrollableScrollPhysics(), // حذف اسکرول
+                          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 3,
+                            crossAxisSpacing: 6,
+                            mainAxisSpacing: 6,
+                            childAspectRatio:
+                                2.3, // باعث میشه قطعه کمی کشیده‌تر و کوچیک‌تر بشه
+                          ),
                           itemCount: _options.length,
                           itemBuilder: (context, index) {
                             return GestureDetector(
@@ -208,6 +209,7 @@ class _FirstPageState extends State<FirstPage> {
                         ),
                       ),
                     ),
+
                     Expanded(
                       child: Row(
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
